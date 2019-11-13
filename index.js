53cae0cfb77b7187f732c4a19f841030eb1a2c15
boisterous-sailor-3516 <christopher.nh.chen@gmail.com>
2019-11-13 03:02:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function breakOut(array, changeValue, stopValue) {
   array.forEach(changeValue => {
     if (changeValue[i] === stopValue[i]) {
-      break;
+      
     }
     return array;
   })
